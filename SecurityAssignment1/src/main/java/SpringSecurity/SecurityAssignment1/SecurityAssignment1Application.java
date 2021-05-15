efa3253816c2e9f7b7e6827e6075ae3ac074845c
Anuragini <anuraginipathak0@gmail.com>
2021-05-15 22:49:05 +0530

    security

---
+++ SecurityAssignment1/src/main/java/SpringSecurity/SecurityAssignment1/SecurityAssignment1Application.java
@@ -0,0 +1,13 @@
+package SpringSecurity.SecurityAssignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityAssignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityAssignment1Application.class, args);
+	}
+
+}
