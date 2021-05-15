dc23e19945f656620cdd2cec3b878dc62a6bbaa7
Anuragini <anuraginipathak0@gmail.com>
2021-05-15 22:37:15 +0530

    Security

---
+++ SpringSecurityAssignment1/src/main/java/SpringSecurity/SpringSecurityAssignment1/SpringSecurityAssignment1Application.java
@@ -0,0 +1,13 @@
+package SpringSecurity.SpringSecurityAssignment1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAssignment1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAssignment1Application.class, args);
+	}
+
+}
---
+++ SpringSecurityAssignment1/src/test/java/SpringSecurity/SpringSecurityAssignment1/SpringSecurityAssignment1ApplicationTests.java
@@ -0,0 +1,13 @@
+package SpringSecurity.SpringSecurityAssignment1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAssignment1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
