2ab8652723e4627d058725dc8589aa37e78bcf5c
Anuragini <anuraginipathak0@gmail.com>
2021-05-15 12:10:39 +0530

    SpringSecurity

---
+++ SpringSecurity/src/main/java/SpringSecurity/SpringSecurity/App.java
@@ -0,0 +1,13 @@
+package SpringSecurity.SpringSecurity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
